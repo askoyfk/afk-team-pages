e0eea17eb2251cb01d6bf849bb26da1cf81e7a12
Victor Nystad <vnystad@gmail.com>
2014-09-20 18:31:54 +0200

    konverter kontakter-fanen i regnearket til contacts.yml

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var Promise = require('es6-promise').Promise;
 var GoogleSpreadsheet = require("google-spreadsheet");
 var afk_sheet = new GoogleSpreadsheet('1svnSp174idz6UiibQbdYqOO6wGH1tbPAxANK6TVZKHc');
+var convert = require('gulp-convert');
+var file = require('gulp-file');
 
 
 gulp.task('default', function () {
@@ -19,6 +21,31 @@ gulp.task('default', function () {
                 })
             }
         })
-    }).then(console.log.bind(console));
+    })
+    .then(function(data) {
+
+        var contacts = [];
+        data.contacts.getRows(1, function(err, rows) {
+            rows.forEach(function(row) {
+                contacts.push({
+                    name: row.navn,
+                    phone: row.telefon,
+                    email: row.epost
+                });
+            });
+
+            file('contacts.json', JSON.stringify(contacts))
+                .pipe(convert({
+                    from: 'json',
+                    to: 'yml'
+                }))
+                .pipe(gulp.dest('contacts'))
+        });
+
+    })
+    .then(console.log.bind(console))
+    .catch(function(error) {
+        console.log(new Error(error))
+    })
 
 });
