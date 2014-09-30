9a68d7823cff46bfb98976c50fd6b984e824c762
Victor Nystad <vnystad@gmail.com>
2014-09-30 10:33:40 +0200

    gruppere barnelag som er delt

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var Promise = require('es6-promise').Promise;
 var GoogleSpreadsheet = require("google-spreadsheet");
 var afk_sheet = new GoogleSpreadsheet('1svnSp174idz6UiibQbdYqOO6wGH1tbPAxANK6TVZKHc');
 var convert = require('gulp-convert');
-var file = require('gulp-file');
+var File = require('gulp-file');
 var insert = require('gulp-insert');
 var replace = require('gulp-ext-replace');
 var del = require('del');
+var beautify = require('gulp-beautify');
+var tap = require('gulp-tap');
 
 var useless = ['_xml', 'id', 'title', 'content', '_links', 'save', 'del'];
 
@@ -35,7 +37,7 @@ gulp.task('contacts', ['clean:contacts'], function () {
                 contacts.push(row);
 
             });
-            file('contacts.json', JSON.stringify(contacts))
+            File('contacts.json', JSON.stringify(contacts))
                 .pipe(convert({
                     from: 'json',
                     to: 'yml'
@@ -77,29 +79,35 @@ gulp.task('teams', ['clean:teams'], function () {
 
                 teams.push(row);
 
-                var filename = row.hovedlag.toLowerCase() + ' ' + row.lagnavniturnering.toLowerCase();
-
-                filename = filename.replace(/[æøå\/]/g, function(m) {
-                    return {
-                        '/' : '-',
-                        'æ' : 'a',
-                        'ø' : 'o',
-                        'å' : 'a'
-                    }[m];
-                }).replace(/\s/g, '-');
-
-
-                file(  filename + '.json', JSON.stringify(row))
-                    .pipe(convert({
-                        from: 'json',
-                        to: 'yml'
-                    }))
-                    .pipe(insert.wrap('---\n', '---\n'))
-                    .pipe(replace('.md'))
-                    .pipe(gulp.dest('teams/' + row.kategori))
+                // var filename = row.hovedlag.toLowerCase() + ' ' + row.lagnavniturnering.toLowerCase();
+                //
+                // filename = filename.replace(/[æøå\/]/g, function(m) {
+                //     return {
+                //         '/' : '-',
+                //         'æ' : 'a',
+                //         'ø' : 'o',
+                //         'å' : 'a'
+                //     }[m];
+                // }).replace(/\s/g, '-');
+                //
+                //
+                // file(  filename + '.json', JSON.stringify(row))
+                //     .pipe(convert({
+                //         from: 'json',
+                //         to: 'yml'
+                //     }))
+                //     .pipe(insert.wrap('---\n', '---\n'))
+                //     .pipe(replace('.md'))
+                //     .pipe(gulp.dest('teams/' + row.kategori))
             });
 
-            file('teams.json', JSON.stringify(teams))
+            File('teams.json', JSON.stringify(teams))
+                .pipe(beautify({
+                    indent: 2
+                }))
+                .pipe(gulp.dest('./data'));
+
+            File('teams.json', JSON.stringify(teams))
                 .pipe(convert({
                     from: 'json',
                     to: 'yml'
@@ -112,7 +120,47 @@ gulp.task('teams', ['clean:teams'], function () {
     .catch(function(error) {
         console.log(new Error(error))
     })
-})
+});
+
+gulp.task('merge:teams', function (cb) {
+    gulp.src('./data/teams.json')
+        .pipe(tap(function (file, t) {
+
+            var data = JSON.parse(file.contents.toString());
+
+            var teams = [];
+
+            data.forEach(function (team) {
+
+                var name = team.lagnavn;
+
+                var group = teams.filter(function(element) {
+                    return element.name === name;
+                })[0] || {
+                    name : name,
+                    category : team.kategori,
+                    teams : []
+                };
+
+                group.teams.push({
+                    name: team.lagnavniturnering,
+                    kontaktperson: team.kontaktperson,
+                    fiksid: team.fiksid || null
+                });
+
+                if (group.teams.length === 1) {
+                    teams.push(group);
+                };
+
+            });
+
+            file.contents = new Buffer(JSON.stringify(teams));
+
+
+        }))
+        .pipe(beautify({ indent: 2 }))
+        .pipe(gulp.dest('./groups/'));
+});
 
 
 gulp.task('clean:teams', function(cb) {
